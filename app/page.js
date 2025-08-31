79c9e58150e9d60cb9fcf7bd8fd948bf97c913a6
ankitabhijeet <62912580+ankitabhijeet@users.noreply.github.com>
2025-09-01 02:04:59 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 "use client";
 import React from 'react';
+import Image from 'next/image'
 import { useRouter } from 'next/navigation';
 import { ArrowRight, Mic, Brain, Target, CheckCircle } from 'lucide-react';
 
