7604a4988c8c87833a71c4d31c1c941d24b6e642
ankitabhijeet <62912580+ankitabhijeet@users.noreply.github.com>
2025-09-01 01:59:20 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -41,8 +41,8 @@ export default function Home() {
       {/* Header */}
       <header className="bg-white border-b border-gray-100">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex justify-end items-center py-6">
-           
+          <div className="flex justify-center items-center py-6">
+            <Image src={'/logo.svg'} width={160} height={100} alt="logo"/>
             <button
               onClick={handleGetStarted}
               className="bg-black text-white px-6 py-2 rounded-lg hover:bg-gray-800 transition-colors duration-200 font-medium"
